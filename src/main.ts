4d83c85ed12c0829314bf14658cd4efe3210b0bd
zhengke <zhengke996@163.com>
2025-01-01 21:41:06 +0800

    add: 封装axios

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from "pinia";
 import router from "./router";
 
 import "virtual:uno.css";
-import 'normalize.css'
+import "normalize.css";
 const pinia = createPinia();
 
 createApp(App).use(pinia).use(router).mount("#app");
