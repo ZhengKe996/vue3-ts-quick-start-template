ad3592fada8d0d24a10202a42bdbe7cd2e38aad3
zhengke <zhengke996@163.com>
2025-01-19 19:48:23 +0800

    build: 添加 pinia 插件实现持久化状态
    - 在 package.json 中添加 pinia-plugin-persistedstate 依赖
    - 版本为 ^4.2.0

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import "virtual:uno.css";
 import "normalize.css";
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 createApp(App).use(pinia).use(router).mount("#app");
